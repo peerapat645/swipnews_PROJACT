07e22854017e74642cd63855fe972d4064945b64
peerapat645 <peerapat.phon@ku.th>
2025-09-16 12:31:06 +0700

    Add files via upload

---
+++ swipnews/gui/accountsingup.java
@@ -0,0 +1,10 @@
+package gui;
+import gui.set.popup;
+
+public class accountsingup extends popup {
+    public accountsingup() {
+        super();
+        setTitle("Author SignUp | SwipNews"); // ตั้งชื่อหน้าต่าง
+    }
+    
+}
